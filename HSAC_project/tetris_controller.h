2c5ccc5fc336a31846c99bf9aba136466e8bc1af
정래경 <dudxn04@gmail.com>
2023-08-22 19:40:40 +0900

    edit tetris_controller.h

--- HSAC_project/tetris_controller.h
+++ HSAC_project/tetris_controller.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef controller
+#define controller
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <stdbool.h>
+
+#include<mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+#endif // !controller
